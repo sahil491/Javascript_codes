d948c5025d06c61c70868cd3399dbe6f876d745d
sahil491 <sahusahil02@gmail.com>
2022-01-17 12:44:53 +0530

    this is second file

---
+++ array.js
@@ -0,0 +1,8 @@
+var student = ["Mohit", "Maths", "12th", "78"];
+
+  console.log(student);
+
+//===================Second array=============================
+  var Person = ["Name:-Mohit", "Address:-Karond", "Mobile_no:-7275865485", "House_no.:-78"];
+
+  console.log(Person);
---
+++ object.js
@@ -0,0 +1,56 @@
+//=======================First object================================
+var bike = {
+    boke_name : 'Access 125',
+    Company_name : 'Suzuki',
+    Owner_name : 'Mohti sahu',
+    RC : 'miod45sw5',
+    Color : 'Black',
+    Insurance : '1452hn5n4',
+    Type : '2 wheeler',
+    speed : '210',
+    Gear : '6'
+
+}
+console.log(bike);
+//=======================Second object=================================
+var student = {
+    Name : "Mohit sahu",
+    Roll_no : "0110",
+    Subject  : "PCM",
+    Class : "12th",
+    Gender :  "Male",
+    Time : "Morning",
+    Fees : "10500",
+    Room_no : "4"
+}
+console.log(student);
+
+
+
+//========================Create objects + array=======================================
+
+var multiple_object = [
+    {
+        Name : "Mohit sahu",
+        Roll_no : "0110",
+        Subject  : "PCM",
+        Class : "12th",
+        Gender :  "Male",
+        Time : "Morning",
+        Fees : "10500",
+        Room_no : "4"
+
+    },
+    {
+        Name : "Rohit singh",
+        Roll_no : "0120",
+        Subject  : "PCM",
+        Class : "12th",
+        Gender :  "Male",
+        Time : "Morning",
+        Fees : "10600",
+        Room_no : "6"
+
+}
+]
+console.log(multiple_object[1].Roll_no);
